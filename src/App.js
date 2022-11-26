1fe08b24f0c700110869405d0a58734634c9aa40
gersonkaitor <gersonkaitor@gmail.com>
2022-11-26 18:19:53 +0800

    navbar component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ForgotPassword from "./pages/ForgotPassword";
 
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <>
@@ -18,8 +20,8 @@ function App() {
             <Route path="/sign-up" element={<SignUp />} />
             <Route path="/forgot-password" element={<ForgotPassword />} />
           </Routes>
+      <Navbar/>
       </Router>
-      {/* Navbar */}
     </>
   );
 }
