a61358af24446ae38e77a7d60a6a423aa43de892
gersonkaitor <gersonkaitor@gmail.com>
2022-11-28 18:27:23 +0800

    display and update user setails

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { getAuth } from "firebase/auth";
+import { useNavigate} from "react-router-dom";
+import { getAuth, updateProfile } from "firebase/auth";
+import { updateDoc, doc } from "firebase/firestore";
+import { db } from "../firebase.config";
+import { toast } from 'react-toastify'
 
 function Profile() {
   const auth = getAuth();
+  const [changeDetails, setChangeDetails] = useState(false);
   const [formData, setFormData] = useState({
     name: auth.currentUser.displayName,
     email: auth.currentUser.email,
   });
 
-  const { name, email } = formData
+  const { name, email } = formData;
 
   const navigate = useNavigate();
 
@@ -18,16 +22,75 @@ function Profile() {
     navigate("/");
   };
 
+  const handleSubmit = async () => {
+    try {
+        if(auth.currentUser.displayName !== name){
+        //update in firestore
+        const userRef = doc(db,'users', auth.currentUser.uid)
+        await updateDoc(userRef, {
+          name
+        })
+
+       // update display name in firebase
+        await updateProfile(auth.currentUser,{
+          displayName: name
+        })
+      }
+    } catch (error) {
+      toast.error('Could not update profile details')
+    }
+  };
+
+  const handleChange = (e) =>{
+    setFormData((prevState) =>({
+      ...prevState,
+      [e.target.id] : e.target.value
+    }))
+  }
+
   return (
     <div className="profile">
       <header className="profileHeader">
-        <p className="pageHeader">
-          {name}
-        </p>
-          <button type="button" onClick={onLogout} className="logOut">
-            Logout
-          </button>
+        <p className="pageHeader">My Profile {name}</p>
+        <button type="button" onClick={onLogout} className="logOut">
+          Logout
+        </button>
       </header>
+      <main>
+        <div className="profileDetailsHeader">
+          <p className="profileDetailsText">Personal Details</p>
+          <p
+            className="changePersonalDetails"
+            onClick={() => {
+              changeDetails && handleSubmit();
+              setChangeDetails((prevState) => !prevState);
+            }}
+          >
+            {changeDetails ? "done" : "change"}
+          </p>
+        </div>
+
+        <div className="profileCard">
+          <form>
+            <input
+              type="text"
+              id="name"
+              className={!changeDetails ? "profileName" : "profileNameActive"}
+              disabled={!changeDetails}
+              value={name}
+              onChange={handleChange}
+            />
+            <input
+              type="text"
+              id="email"
+              className={!changeDetails ? "profileEmail" : "profileEmailActive"}
+              disabled={!changeDetails}
+              value={email}
+              onChange={handleChange}
+            />
+          </form>
+        </div>
+      </main>
     </div>
   );
 }
