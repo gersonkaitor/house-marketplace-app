e0522f75971bb4284b1aad89ac4b2b5f613642b2
gersonkaitor <gersonkaitor@gmail.com>
2022-11-28 09:58:20 +0800

    user sign in

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import { ReactComponent as ArrowRightIcon } from "../assets/svg/keyboardArrowRightIcon.svg";
 import visibilityIcon from "../assets/svg/visibilityIcon.svg";
 
@@ -15,10 +16,30 @@ function SignIn() {
   const navigate = useNavigate();
 
   const handleChange = (e) => {
-    setFormData((prevState) =>({
+    setFormData((prevState) => ({
       ...prevState,
-      [e.target.id] : e.target.value
+      [e.target.id]: e.target.value,
     }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const auth = getAuth();
+
+      const userCredential = await signInWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      if (userCredential.user) {
+        navigate("/");
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -27,7 +48,7 @@ function SignIn() {
         <header>
           <p className="pageHeader">Welcome Back!</p>
         </header>
-        <form>
+        <form onSubmit={handleSubmit}>
           <input
             type="email"
             className="emailInput"
@@ -53,15 +74,13 @@ function SignIn() {
               onClick={() => setShowPassword((prevState) => !prevState)}
             />
           </div>
-          <Link to='/forgot-password' className="forgotPasswordLink">
+          <Link to="/forgot-password" className="forgotPasswordLink">
             Forgot Password
           </Link>
           <div className="signInBar">
-            <p className="signInText">
-              Sign In
-            </p>
-            <button className="signInButton">
-              <ArrowRightIcon fill='#fff' width="34px" height="34px"/>
+            <p className="signInText">Sign In</p>
+            <button type="submit" className="signInButton">
+              <ArrowRightIcon fill="#fff" width="34px" height="34px" />
             </button>
           </div>
         </form>
