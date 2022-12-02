5391b52a5f68887c615ae2e489b3dc7fad28683d
gersonkaitor <gersonkaitor@gmail.com>
2022-12-02 09:24:35 +0800

    leaflet map

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
 import { getDoc, doc } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 import { db } from "../firebase.config";
@@ -62,30 +63,54 @@ function Listing() {
         </p>
         <p className="listingLocation">{listing.location}</p>
         <p className="listingType">
-            For {listing.type === 'rent' ? "Rent" : "Sale"}
+          For {listing.type === "rent" ? "Rent" : "Sale"}
         </p>
         {listing.offer && (
-            <p className="discountPrice">
-                ${listing.regularPrice - listing.discountedPrice} discount
-            </p>
+          <p className="discountPrice">
+            ${listing.regularPrice - listing.discountedPrice} discount
+          </p>
         )}
 
         <ul className="listingDetailsList">
-            <li>
-                {listing.bedrooms > 1 ? `${listing.bedrooms} Bedrooms` : '1 Bedroom'}
-            </li>
-            <li>
-                {listing.bathrooms > 1 ? `${listing.bathrooms} Bathrooms` : '1 Bathroom'}
-            </li>
-            <li>{listing.parking && 'Parking Spot'}</li>
-            <li>{listing.furnished && 'Furnished'}</li>
+          <li>
+            {listing.bedrooms > 1
+              ? `${listing.bedrooms} Bedrooms`
+              : "1 Bedroom"}
+          </li>
+          <li>
+            {listing.bathrooms > 1
+              ? `${listing.bathrooms} Bathrooms`
+              : "1 Bathroom"}
+          </li>
+          <li>{listing.parking && "Parking Spot"}</li>
+          <li>{listing.furnished && "Furnished"}</li>
         </ul>
         <p className="listingLocationTitle">Location</p>
-        {/* Map */}
+
+        <div className="leafletContainer">
+          <MapContainer
+            style={{ height: "100%", width: "100%" }}
+            center={[listing.geolocation.lat, listing.geolocation.lng]}
+            zoom={13}
+            scrollWheelZoom={false}
+          >
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker position={[listing.geolocation.lat, listing.geolocation.lng]}>
+              <Popup>{listing.location}</Popup>
+            </Marker>
+          </MapContainer>
+        </div>
+
         {auth.currentUser?.uid !== listing.userRef && (
-            <Link to={`/contact/${listing.userRef}?listingName=${listing.name}`} className="primaryButton">
-                Contact Landlord
-            </Link>
+          <Link
+            to={`/contact/${listing.userRef}?listingName=${listing.name}`}
+            className="primaryButton"
+          >
+            Contact Landlord
+          </Link>
         )}
       </div>
     </main>
